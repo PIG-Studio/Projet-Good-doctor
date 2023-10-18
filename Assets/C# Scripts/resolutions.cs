fb9140fa741869893e22f548b445a81cea314ad3
wambsta <wambsta@super-kartoffel.lan>
2023-10-18 21:06:18 +0200

    Implementation nouveaux params

---
+++ Assets/C# Scripts/resolutions.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ResolutionsController : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+
+    private Resolution[] resolutions;
+    private List<Resolution> filteredResolutions;
+    
+    private int currentResolutionIndex = 0;
+    
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        filteredResolutions = new List<Resolution>();
+
+        resolutionDropdown.ClearOptions();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            filteredResolutions.Add(resolutions[i]);
+        }
+
+        List<string> options = new List<string>();
+        for (int i = 0; i < filteredResolutions.Count; i++)
+        {
+            string resolutionOption = filteredResolutions[i].width +
+                                      "x" + filteredResolutions[i].height;
+            options.Add(resolutionOption);
+            if (filteredResolutions[i].width == Screen.width && filteredResolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = filteredResolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreenMode, Screen.currentResolution.refreshRateRatio);
+    }
+}
