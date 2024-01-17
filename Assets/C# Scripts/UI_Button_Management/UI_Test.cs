11579ffd2d81c9c398112bb064eb3def4bd917a8
Arthur Wambst <wambst.arthur2@gmail.com>
2024-01-17 14:53:53 +0100

    added first UI scripts

---
+++ Assets/C# Scripts/UI_Button_Management/UI_Test.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Test : UI_Prefab
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        test();
+    }
+
+}
