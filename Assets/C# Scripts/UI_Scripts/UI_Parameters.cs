68523f1fe24f8f6ce3720a4e80178e24455e2ed9
Arthur Wambst <wambst.arthur2@gmail.com>
2024-02-01 14:57:55 +0100

    save

---
+++ Assets/C# Scripts/UI_Scripts/UI_Parameters.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Parameters : UI_Prefab
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dictionary<string, GameObject> dicoRender =
+            new Dictionary<string, GameObject>
+                () { };
+        dicoRender["Retour"] = ButtonChangeScene("Retour", GameVariables.SceneName_Last, -300f, 100f, 150f, 50f, GameVariables.SceneName_Last);
+        Instancier("UI_ParamCanvas", dicoRender); 
+    }
+}
