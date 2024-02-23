06d90635695228388559c47a90f2ab922c1c9074
Arthur Wambst <wambst.arthur2@gmail.com>
2024-02-23 15:36:56 +0100

    save

--- Assets/C# Scripts/Desks.cs
+++ Assets/C# Scripts/Desks.cs
@@ -24,6 +24,11 @@ public static class DesksConvert
             { Desks.BaseDesk, "DESK_Base" },
             { Desks.ChirDesk, "DESK_Chirurgien" }
         };
+    
+    public static bool ValidString(string input)
+    {
+        return DictStringToDesk.ContainsKey(input);
+    }
 
     public static Desks ToDesk(string input)
     {
