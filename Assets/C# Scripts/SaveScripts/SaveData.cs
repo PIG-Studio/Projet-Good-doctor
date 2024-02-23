99823a24d74dd58c4680ec844ff2b3caecf8e784
Alexiis <alexisragot27@gmail.com>
2024-02-23 11:06:28 +0100

    setup de git + changer update method save

--- Assets/C# Scripts/SaveScripts/SaveData.cs
+++ Assets/C# Scripts/SaveScripts/SaveData.cs
@@ -9,8 +9,43 @@ public class SaveData : MonoBehaviour
 {
     
         public string sceneName = SceneManager.GetActiveScene().name ;
-        //public float playerHealth;
+        //ublic float playerHealth;
         //public int playerScore;
+        //public (float,float) position;
+        //
+        
+        
+        /// <summary>
+        /// met GameVariables.SaveName a 'input' si le nom est valide
+        /// </summary>
+        /// <param name="input">new save name, validity will be tested</param>
+        /// <returns>bool => input's validity to be the new SaveName, checked using SaveLoadMethods.ValidNameToSave</returns>
+        public static bool SetSaveName(string input)
+        {
+        
+                bool valid = SaveLoadMethods.ValidNameToSave(input);// TODO : && nom pas en doublon 
+                if (valid) { GameVariables.SaveName = input; }
+                else { GameVariables.SaveName = ""; }
+        
+                Debug.Log("GameVariables.SaveName = " + GameVariables.SaveName + " (FROM : GameVariables, apres modif de la valeur");
+        
+                return valid; 
+        }
+        
+        /// <summary>
+        /// create a dictionary with values needed to save a game, and passes it to SaveLoadMethods.WriteSaveGame
+        /// </summary>
+        /// <returns>void</returns>
+        public void SaveGame()
+        {
+                Dictionary<string, string> input = new Dictionary<string, string>();
+                input["DeskName"] = DesksConvert.DeskToString(GameVariables.DeskName);
+                input["SaveName"] = GameVariables.SaveName;
+                input["SceneName"] = GameVariables.SceneName_Current;
+
+                SaveLoadMethods.WriteSaveGame(input);
+        }
+        
     
 }
 
