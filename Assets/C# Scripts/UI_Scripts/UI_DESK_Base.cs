f7c465fea47290fc8a3c7895743ae6280ea83345
Arthur Wambst <wambst.arthur2@gmail.com>
2024-02-23 16:21:10 +0100

    save

---
+++ Assets/C# Scripts/UI_Scripts/UI_DESK_Base.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_DESK_Base : UI_Prefab
+{
+    private void Start()
+    {
+        Dictionary<string, GameObject> dicoRender =
+            new Dictionary<string, GameObject>
+                () { };
+        dicoRender["Parametres"] = ButtonChangeScene("Parameters", "Parametres", -300f, 100f, 150f, 50f, "Parameters");
+        dicoRender["Quit"] = ButtonQuit("Quit", "Quit", -300f, -100f, 150f, 50f);
+        dicoRender["SaveGame"] = ButtonSave("SaveGame", "Save Game", 300f, 100f, 150f, 50f);
+        Instancier("UI_DESK_BaseCanvas", dicoRender);
+    }
+}
