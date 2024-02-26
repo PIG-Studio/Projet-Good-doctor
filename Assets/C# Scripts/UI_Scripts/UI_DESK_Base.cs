70e1fc5fc5171d0dc167a9ea00d2510fff629914
Arthur Wambst <wambst.arthur2@gmail.com>
2024-02-26 20:39:31 +0100

    save

--- Assets/C# Scripts/UI_Scripts/UI_DESK_Base.cs
+++ Assets/C# Scripts/UI_Scripts/UI_DESK_Base.cs
@@ -1,4 +1,7 @@
 using System.Collections.Generic;
+using System.Threading.Tasks.Sources;
+using Alteruna;
+using UnityEditor;
 using UnityEngine;
 
 public class UI_DESK_Base : UI_Prefab
@@ -11,6 +14,11 @@ public class UI_DESK_Base : UI_Prefab
         dicoRender["Parametres"] = ButtonChangeScene("Parameters", "Parametres", -300f, 100f, 150f, 50f, "Parameters");
         dicoRender["Quit"] = ButtonQuit("Quit", "Quit", -300f, -100f, 150f, 50f);
         dicoRender["SaveGame"] = ButtonSave("SaveGame", "Save Game", 300f, 100f, 150f, 50f);
-        Instancier("UI_DESK_BaseCanvas", dicoRender);
+        
+        GameObject canv = Instancier("UI_DESK_BaseCanvas", dicoRender);
+        
+        GameObject prefabMenuRoom = Resources.Load<GameObject>("Room Menu");
+        GameObject menuRoom = Instantiate(prefabMenuRoom);
+
     }
 }
