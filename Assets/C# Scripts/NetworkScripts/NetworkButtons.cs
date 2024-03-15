42d1b189d2c37c3471cbc47d6101e9d2a80ff370
Arthur Wambst <wambst.arthur2@gmail.com>
2024-03-15 17:07:24 +0100

    save

--- Assets/C# Scripts/NetworkScripts/NetworkButtons.cs
+++ Assets/C# Scripts/NetworkScripts/NetworkButtons.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class NetworkButtons : MonoBehaviour
 {
+    public GameObject soloPlayer; //player to activate when we stop hosting or disconnect
     private void OnGUI()
     {
         GUILayout.BeginArea(new Rect(10, 10, 300, 300));
@@ -13,10 +14,12 @@ public class NetworkButtons : MonoBehaviour
             Debug.Log(GameVariables.SceneName_Current);
             if (DesksConvert.ValidString(GameVariables.SceneName_Current)) // Si la scene actuelle est le bureau
             {
-                if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+                if (GUILayout.Button("Host")) {NetworkManager.Singleton.StartHost();soloPlayer.SetActive(false);}
                 //if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
-                if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+                if (GUILayout.Button("Client")) {NetworkManager.Singleton.StartClient();soloPlayer.SetActive(false);}
             }
+            if (GameVariables.SceneName_Current == "MapHospital") {soloPlayer.SetActive(true);}
+            else { soloPlayer.SetActive(false); }
         }
         else
         {
@@ -25,8 +28,8 @@ public class NetworkButtons : MonoBehaviour
                 GUILayout.Label("Server");
                 GUILayout.Label($"ID: {NetworkManager.Singleton.LocalClientId}");
                 GUILayout.Label($"IP : {NetworkManager.Singleton.ConnectedHostname}");
-                GUILayout.Label($"Connected : {NetworkManager.Singleton.ConnectedClients.Keys.ToString()}");
-                if (GUILayout.Button("Shutdown")) {NetworkManager.Singleton.Shutdown();}
+                GUILayout.Label($"Connected : {NetworkManager.Singleton.ConnectedClients.Count}");
+                if (GUILayout.Button("Shutdown")) {NetworkManager.Singleton.Shutdown(); soloPlayer.SetActive(true);}
             }
             else if (NetworkManager.Singleton.IsClient)
             {
