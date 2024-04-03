e71b556796e1cd284c9cf9a95b8bc501e2be6e29
Arthur Wambst <wambst.arthur2@gmail.com>
2024-04-03 16:29:54 +0200

    $date

--- Assets/C# Scripts/NetworkScripts/NetworkVariables.cs
+++ Assets/C# Scripts/NetworkScripts/NetworkVariables.cs
@@ -2,26 +2,23 @@ using GameCore;
 using Unity.Netcode;
 using UnityEngine;
 
-namespace Network
+[GenerateSerializationForType(typeof(Desk))]
+public class NetworkVariables : NetworkBehaviour
 {
-    [GenerateSerializationForGenericParameter(0)]
-    public class Variables<T> : NetworkBehaviour where T : Desk
+    private readonly NetworkVariable<Desk> inv = new(writePerm:NetworkVariableWritePermission.Owner);
+
+    void Update()
     {
-        private readonly NetworkVariable<Desk> inv = new(writePerm:NetworkVariableWritePermission.Owner);
-
-        void Update()
+        if (!NetworkManager.Singleton.IsClient)
         {
-            if (!NetworkManager.Singleton.IsClient)
-            {
-                inv.Value = Desks.Desk_Base;
-            }
-            else
-            {
-                Desks.Desk_Base = inv.Value;
-            }
-            Debug.Log(Desks.Desk_Base.Inventory.Slots[0].Amount);
+            inv.Value = Desks.Desk_Base;
         }
-
-
+        else
+        {
+            Desks.Desk_Base = inv.Value;
+        }
+        Debug.Log(Desks.Desk_Base.Inventory.Slots[0].Amount);
     }
+
+
 }
