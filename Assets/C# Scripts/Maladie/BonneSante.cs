541e17e28d99083fdedb5e27d42280dbcc3a2b09
clementine.frenois <clementine.frenois@epita.fr>
2024-04-25 17:54:40 +0200

    ffefff

---
+++ Assets/C# Scripts/Maladie/BonneSante.cs
@@ -0,0 +1,8 @@
+﻿namespace C__Scripts.Maladie
+{
+    public class BonneSante : Maladie
+    {
+        public BonneSante() : base("En Bonne Santé", (60,80),(35,37), true, (0,9))
+        { }
+    }
+}
