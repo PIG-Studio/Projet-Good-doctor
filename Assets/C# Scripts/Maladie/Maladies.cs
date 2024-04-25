59fb63f4853962ac230e32bd30792b7e7f5fb6f5
clementine.frenois <clementine.frenois@epita.fr>
2024-04-25 17:08:00 +0200

    save

---
+++ Assets/C# Scripts/Maladie/Maladies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C__Scripts.Maladie
+{
+    public static class Maladies
+    {
+        public static SyndromePorcelaine PORCELAINE()
+        {
+            return new SyndromePorcelaine();
+        }
+
+        public static Maladie[] Maladiess = new[]
+        {
+            (PORCELAINE())
+        };
+
+    }
+}
