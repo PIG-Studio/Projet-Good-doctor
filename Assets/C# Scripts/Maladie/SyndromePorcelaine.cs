b9bd38af880dd281ebda2828b8e2d48e4a6f213b
clementine.frenois <clementine.frenois@epita.fr>
2024-04-25 16:39:18 +0200

    save

---
+++ Assets/C# Scripts/Maladie/SyndromePorcelaine.cs
@@ -0,0 +1,9 @@
+﻿namespace C__Scripts.Maladie
+{
+    public class SyndromePorcelaine : Maladie
+    {
+        public SyndromePorcelaine(string "Syndrome de Porcelaine", (uint, uint) freqcar, (uint, uint) temp, bool normalADN, (uint, uint) depression) : base(name, freqcar, temp, normalADN, depression)
+        {
+        }
+    }
+}
