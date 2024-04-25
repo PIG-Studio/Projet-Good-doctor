be2752f99145cddd890d97a189cd8b51d96dd624
clementine.frenois <clementine.frenois@epita.fr>
2024-04-25 15:56:59 +0200

    save

---
+++ Assets/C# Scripts/Maladie/Maladie.cs
@@ -0,0 +1,20 @@
+﻿using Interfaces;
+
+namespace C__Scripts.Maladie
+{
+    public abstract class Maladie : IMaladie
+    {
+        public string Name { get; set; }
+        public (uint, uint) FreqCar { get; set; }
+        public (uint, uint) Temperature { get; set; }
+        public bool ADN_Normalc { get; set; }
+
+        public Maladie(string name, (uint, uint) freqcar)
+        {
+            Name = name;
+            FreqCar = freqcar;
+            
+        }
+
+    }
+}
