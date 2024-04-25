dcc73e0ea06e8c690d8cf62c53deb53b1d0f8db7
clementine.frenois <clementine.frenois@epita.fr>
2024-04-25 18:10:04 +0200

    merge

--- Assets/C# Scripts/Maladie/Maladie.cs
+++ Assets/C# Scripts/Maladie/Maladie.cs
@@ -10,6 +10,7 @@ namespace C__Scripts.Maladie
         public bool NormalAdn { get; set; }
         
         public (uint, uint) Depression { get; set; }
+
         
         public Maladie(string name, (uint, uint) freqcar, (uint,uint) temp, bool normalADN, (uint, uint) depression)
         {
