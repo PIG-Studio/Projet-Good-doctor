a23fc197eb15841ba366c06c57a4e2dddfc68309
Alexiis <alexisragot27@gmail.com>
2024-04-25 17:10:17 +0200

    Patients

--- Assets/C# Scripts/Patient/Patients.cs
+++ Assets/C# Scripts/Patient/Patients.cs
@@ -3,6 +3,9 @@ namespace C__Scripts.Patient
     public class Patients
     {
         // choisis aleatoire maladie , mensonge ou non , pour plus tard Skin/Nom/Catchphrase
-        
+        //choisis en randint dans maladie.array[maladies] 
+        //si pas atteint le nombre max de porteur de maladie alors on creer sinon on rerandint
+        //On Creer (ADN , temp , freq , mood , depression , ADNormale ) en fct des symptomes la maladie (randint des intervalles)
+        // on random lying , catchphrase
     }
 }
