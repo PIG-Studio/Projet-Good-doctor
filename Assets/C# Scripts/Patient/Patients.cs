b3d8ef4d9407d42e3a71d7dd852065d71868114e
Alexiis <alexisragot27@gmail.com>
2024-04-25 17:54:37 +0200

    Debut random

--- Assets/C# Scripts/Patient/Patients.cs
+++ Assets/C# Scripts/Patient/Patients.cs
@@ -1,11 +1,36 @@
+using C__Scripts.Maladie;
+using UnityEngine;
+using Random = System.Random;
+
 namespace C__Scripts.Patient
 {
-    public class Patients
+    using static Random;
+
+    public class Patients : PNJ.Patient
     {
         // choisis aleatoire maladie , mensonge ou non , pour plus tard Skin/Nom/Catchphrase
         //choisis en randint dans maladie.array[maladies] 
         //si pas atteint le nombre max de porteur de maladie alors on creer sinon on rerandint
         //On Creer (ADN , temp , freq , mood , depression , ADNormale ) en fct des symptomes la maladie (randint des intervalles)
         // on random lying , catchphrase
+        
+        public Patients(Maladie.Maladie sickness, string adn, bool adNormal, uint depression, uint temperature, uint freqCar, string catchPhrase, bool lie, int mood, string phrase, Sprite skin, string name, Vector2 position) : base(sickness, adn, adNormal, depression, temperature, freqCar, catchPhrase, lie, mood, phrase, skin, name, position)
+        {
+        }
+
+        public PNJ.Patient GenPat()
+        {
+            Random rnd = new Random();
+            int lie = rnd.Next(0,1);
+            bool lies = lie == 1;
+            if (lies == false)
+            {
+                
+            }
+            else
+            {
+                int i = rnd.Next(0, Maladie.Maladies.Maladiess.Length - 1);
+            }
+        }
     }
 }
