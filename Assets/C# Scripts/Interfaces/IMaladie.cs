24151df6b75ae4b598e1e36f9b7859c55b072faa
clementine.frenois <clementine.frenois@epita.fr>
2024-04-25 15:56:59 +0200

    save

---
+++ Assets/C# Scripts/Interfaces/IMaladie.cs
@@ -0,0 +1,10 @@
+﻿namespace Interfaces
+{
+    public interface IMaladie
+    {
+        string Name { get; set; }
+        (uint, uint) FreqCar { get; set; }
+        (uint, uint) Temperature { get; set; }
+        bool ADN_Normalc { get; set; }
+    }
+}
