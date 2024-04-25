bbbe6ef288ba8f5ec4ab14fc830d0e4201be279f
clementine.frenois <clementine.frenois@epita.fr>
2024-04-25 18:16:33 +0200

    zuhg

--- Assets/C# Scripts/Maladie/Maladie.cs
+++ Assets/C# Scripts/Maladie/Maladie.cs
@@ -8,8 +8,9 @@ namespace C__Scripts.Maladie
         public (uint, uint) FreqCar { get; set; }
         public (uint, uint) Temperature { get; set; }
         public bool NormalAdn { get; set; }
-        
-        public (uint, uint) Depression { get; set; }
+
+        public (uint, uint) Depression { get; set; }// plus c'est bas plus il est heureux
+
 
         
         public Maladie(string name, (uint, uint) freqcar, (uint,uint) temp, bool normalADN, (uint, uint) depression)
