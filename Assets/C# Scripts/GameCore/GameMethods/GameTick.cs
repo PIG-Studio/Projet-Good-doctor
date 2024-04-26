c89cf14182cef63d151bb256354c68b21d5bec51
Alexiis <alexisragot27@gmail.com>
2024-04-26 15:51:56 +0200

    save

--- Assets/C# Scripts/GameCore/GameMethods/GameTick.cs
+++ Assets/C# Scripts/GameCore/GameMethods/GameTick.cs
@@ -1,16 +1,23 @@
+using CustomScenes;
+using GameCore.GameVAR;
+using GameCore.TypeExpand;
+using Patient;
 using UnityEngine;
 namespace GameCore.GameMethods
 {
     public class GameTick : MonoBehaviour
-    {/*
+    {
         public void Update()
         {
-            if (Variables.SceneName_Current.IsDesk() || Variables.SceneName_Current == Scenes.MAP)
+            if (Variables.SceneNameCurrent.IsDesk() || Variables.SceneNameCurrent == Scenes.MAP)
             {
-                Patients.GenPat();
-                Debug.Log("Patient a l'entrée de l'hôpital !");
+                if (Input.GetKeyDown(KeyCode.P))
+                {
+                    Patients.GenPat();
+                    Debug.Log("Patient a l'entrée de l'hôpital !");
+                }
             }
             
-        }*/
+        }
     }
 }
