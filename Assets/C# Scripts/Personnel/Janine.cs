e1a959df61cddee06598e332814dbb945123bf93
Alexiis <alexisragot27@gmail.com>
2024-04-29 17:31:45 +0200

    Mon Apr 29 17:31:45     2024

---
+++ Assets/C# Scripts/Personnel/Janine.cs
@@ -0,0 +1,50 @@
+using System;
+using Interfaces;
+using UnityEngine;
+
+namespace Personnel
+{
+    public class Janine : IPnj , ISpawn
+    {
+        public Sprite Skin { get; set; }
+        public string Name { get; set; }
+        public Vector2 Position { get; set; }
+
+        public Janine()
+        {
+            //Skin = Skin de janine
+            // First Name = Janine
+            // Surname = son nom de famille
+            // Position = Derriere son bureau
+        }
+
+        /// <summary>
+        /// Fonction qui renvoie une phrase indiquant le reput du joueur
+        /// </summary>
+        public void Reputation(){}
+        
+        /// <summary>
+        /// renvoie une phrase random de Janine
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void talk()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        /// <summary>
+        /// Fonction appeler au debut de la partie qui instantie Janine 
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Spawn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Despawn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/C# Scripts/Personnel/Quorentin.cs
@@ -0,0 +1,54 @@
+using Interfaces;
+using UnityEngine;
+
+namespace Personnel
+{
+    public class Quorentin : IPnj , ISpawn
+    {
+        public Sprite Skin { get; set; }
+        public string Name { get; set; }
+        public Vector2 Position { get; set; }
+
+        public Quorentin()
+        {
+            //Skin = Skin de Quorentin
+            // First Name = Quorentin
+            // Surname = Pas de nom
+            // Position = Dans le labo
+        }
+
+
+        /// <summary>
+        /// Fonction qui Renvoie Depression et Adn du patient dans le bureau
+        /// </summary>
+        public void AnalysePatient()
+        {
+            //Si InDesk.Patient.AnalyseAdn == False alors AnalasyeAdn = True , rajoute à la fiche et renvoie son ADN
+            //SI InDesk.Patient.AnalyseDepression == False  alors AnalyseDepression = True , rajoute à la fiche et renvoie sa Depression
+        }
+        
+        
+        /// <summary>
+        /// Fonction qui renvoie une phrase
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void talk()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// spawn Quorentin au debut de la partie dans le labo
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Spawn()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Despawn()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
