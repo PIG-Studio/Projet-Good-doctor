bafd1455758fecf8afa9595200e772ce7092dbee
Arthur Wambst <wambst.arthur2@gmail.com>
2024-04-30 14:38:20 +0200

    mar. 30 avril 2024 14:38:20 CEST

--- Assets/C# Scripts/PNJ/Pnj.cs
+++ Assets/C# Scripts/PNJ/Pnj.cs
@@ -8,17 +8,17 @@ namespace PNJ
     {
         public NavMeshAgent AgentComp { get; private set; }
 
-        public UnityEngine.Animator AnimatorComp { get; private set; }
+        public Animator AnimatorComp { get; private set; }
         
         public void Start()
         {
             AgentComp = gameObject.GetComponent<NavMeshAgent>();
-            AnimatorComp = gameObject.GetComponent<UnityEngine.Animator>();
+            AnimatorComp = gameObject.GetComponent<Animator>();
         }
 
         public void Update()
         { 
-            AnimatorComp.UpdateAnim(AgentComp.desiredVelocity);
+            AnimatorComp.UpdateAnim(AgentComp.velocity);
         }
     }
 }
