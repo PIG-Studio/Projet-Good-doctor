34d9823c3f370749c8737d1d8bf78b1300569527
clementine.frenois <clementine.frenois@epita.fr>
2024-04-30 16:56:24 +0200

    Tue Apr 30 16:56:24     2024

---
+++ Assets/C# Scripts/InventoryTwo/PickUpItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using InventoryTwo;
+using UnityEngine;
+using static ItemsSo;
+
+public class PickUpItem : MonoBehaviour
+{
+    public ItemsSo item;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+           InventoryManager.instance.inventory.Add(item);
+           Destroy(gameObject);
+        }
+    }
+}
+    
