be8bf967ba1bf09334b5359f671648ae64506ce9
Alexiis <alexisragot27@gmail.com>
2024-05-02 16:54:18 +0200

    Thu May  2 16:54:18     2024

--- Assets/C# Scripts/Patient/DialoguePatient.cs
+++ Assets/C# Scripts/Patient/DialoguePatient.cs
@@ -8,6 +8,7 @@ namespace Patient
         public GameObject boiteDialogue; // référence à la boîte de dialogue public static Type Text { get; }
         public TextMeshProUGUI text;
         public Patient patient;
+        public Renderer bubble;
         private string _catchPhrase;
         private string _name;
         private bool _close;
@@ -20,10 +21,14 @@ namespace Patient
         {
             boiteDialogue = gameObject.transform.GetChild(0).gameObject;
             text = boiteDialogue.gameObject.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+            bubble = boiteDialogue.GetComponent<SpriteRenderer>();
             boiteDialogue.SetActive(false); // le dialogue commence masqué
+            bubble.enabled = false;
             _catchPhrase = patient.CatchPhrase;
             _name = patient.Name;
             text.text = _name;
+            Vector3 pos = boiteDialogue.GetComponent<RectTransform>().position;
+            boiteDialogue.GetComponent<RectTransform>().position = new Vector3(pos.x ,pos.y - 1f , pos.z);
         }
 
 
@@ -42,7 +47,13 @@ namespace Patient
         {
             if (other.CompareTag("Player"))
             {
-                _close = false; 
+                _close = false;
+                if (bubble.enabled)
+                {
+                    Vector3 pos = boiteDialogue.GetComponent<RectTransform>().position;
+                    boiteDialogue.GetComponent<RectTransform>().position = new Vector3(pos.x ,pos.y - 1f , pos.z);    
+                }
+                bubble.enabled = false;
                 boiteDialogue.SetActive(false);
                 text.text = _name;
             }
@@ -50,8 +61,11 @@ namespace Patient
 
         private void Update()
         {
-            if (_close && Input.GetKeyDown(KeyCode.T))
+            if (_close && Input.GetKeyDown(KeyCode.T) && bubble.enabled == false)
             {
+                Vector3 pos = boiteDialogue.GetComponent<RectTransform>().position;
+                boiteDialogue.GetComponent<RectTransform>().position = new Vector3(pos.x ,pos.y + 1f , pos.z);
+                bubble.enabled = true;
                 text.text = _catchPhrase;
             }
         }
