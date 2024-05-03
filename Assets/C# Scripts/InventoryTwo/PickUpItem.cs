103bb83925adad06a20a5bc4b843a0ad348a262d
clementine.frenois <clementine.frenois@epita.fr>
2024-05-03 10:17:37 +0200

    Fri May  3 10:17:36     2024

--- Assets/C# Scripts/InventoryTwo/PickUpItem.cs
+++ Assets/C# Scripts/InventoryTwo/PickUpItem.cs
@@ -8,9 +8,11 @@ namespace InventoryTwo
         public ItemsSo item;
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (!CompareTag("Player")) return;
-            InventoryManager.instance.inventory.Add(item);
-            Destroy(gameObject);
+            if (CompareTag("Player"))
+            {
+                InventoryManager.instance.inventory.Add(item);
+                Destroy(gameObject);
+            }
         }
     }
 }
