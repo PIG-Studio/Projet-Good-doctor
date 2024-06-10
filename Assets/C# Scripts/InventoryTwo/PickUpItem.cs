1aa2bee5c07aeb404f46c71e2934845fa2294ceb
clementine.frenois <clementine.frenois@epita.fr>
2024-06-10 18:23:34 +0200

    Mon Jun 10 18:23:34     2024

--- Assets/C# Scripts/InventoryTwo/PickUpItem.cs
+++ Assets/C# Scripts/InventoryTwo/PickUpItem.cs
@@ -9,16 +9,18 @@ namespace InventoryTwo
         public void OnTriggerEnter2D(Collider2D other)
         {
             if (!other.CompareTag("Player")) return;
+            ItemsSo newItem = new ItemsSo(item.title, item.description, item.icon, item.amount, item.isStackable,
+                item.type);
             for (int i = 0; i < InventoryManager.Instance.inventory.Count; i++)
             {
                 if (item.title == InventoryManager.Instance.inventory[i].title && item.isStackable &&
                     InventoryManager.Instance.inventory.Count > 0)
                 {
-                    item.amount += InventoryManager.Instance.inventory[i].amount;
+                    newItem.amount += InventoryManager.Instance.inventory[i].amount;
                     InventoryManager.Instance.inventory.Remove(InventoryManager.Instance.inventory[i]);
                 }
             }
-            InventoryManager.Instance.inventory.Add(item);
+            InventoryManager.Instance.inventory.Add(newItem);
             Destroy(gameObject);
         }
     }
