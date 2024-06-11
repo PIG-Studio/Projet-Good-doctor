5e7df5babe7478fdd188de0eebdecf15ef65d2ac
Alexiis <alexisragot27@gmail.com>
2024-06-11 09:27:24 +0200

    Tue Jun 11 09:27:24     2024

--- Assets/C# Scripts/GameCore/Methods/GameTick.cs
+++ Assets/C# Scripts/GameCore/Methods/GameTick.cs
@@ -53,12 +53,6 @@ namespace GameCore.Methods
             // Vérification si la scène actuelle n'est pas un bureau ou la carte
             if (!Variable.SceneNameCurrent.IsDesk() && Variable.SceneNameCurrent != Scenes.Map) return;
             if (!Input.GetKeyDown(KeyCode.P)) return;
-            
-            var guillaume = Instantiate(_patient);
-            //Debug.Log(guillaume.Name + " " + guillaume.Depression + " " + guillaume.Sickness + " " +
-            //          guillaume.Adn);
-            // Affichage d'un message pour signaler l'arrivée du patient à l'entrée de l'hôpital (pour débogage)
-            Debug.Log("Patient a l'entrée de l'hôpital !");
         }
     }
 }       
