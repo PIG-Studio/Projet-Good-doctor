faf1f6b3cf4750090258c140a2132517ac98a8b7
clementine.frenois <clementine.frenois@epita.fr>
2024-06-11 16:17:44 +0200

    Tue Jun 11 16:17:44     2024

--- Assets/C# Scripts/InventoryTwo/SlotItem.cs
+++ Assets/C# Scripts/InventoryTwo/SlotItem.cs
@@ -1,4 +1,5 @@
 ﻿using InventoryTwo;
+using InventoryTwo.Desk;
 using InventoryTwo.Player;
 using UnityEngine;
 
@@ -9,5 +10,6 @@ public class SlotItem : MonoBehaviour // va dans dans un componenet de prefab de
     public void ChargeItem()
     {
         InventoryManager.Instance.ChargeItem(itemSlot);
+        DeskInventoryManager.InstanceDIM.ChargeItem(itemSlot);
     }
 }
