2706e545d1a598c773e7ff38042b1852913ae303
clementine.frenois <clementine.frenois@epita.fr>
2024-06-17 01:59:26 +0200

    butonsMenu

--- Assets/C# Scripts/UI_Scripts/UI_Scenes/UIMenu.cs
+++ Assets/C# Scripts/UI_Scripts/UI_Scenes/UIMenu.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks.Sources;
 using UnityEngine;
 using UI_Scripts.UI_Prefab;
 using CustomScenes;
@@ -23,13 +24,13 @@ namespace UI_Scripts.UI_Scenes
                     () ;
 
             // Ajout des elements a afficher
-            dicoRender["Parametres"] = UIPrefabs.BTN_ChangeScene("Parameters", "", -500f, 300f, 100f, 100f,
+            dicoRender["Parametres"] = UIPrefabs.BTN_ChangeScene("Parameters", "", -700f, 400f, 100f, 80f,
                 Scenes.Param, "Button/prettyButton/Gear-Default");
-            dicoRender["Quit"] = UIPrefabs.BTN_Quit("Quit", "Quit", -300f, -100f, 150f, 50f);
-            dicoRender["InputGameName"] = UIPrefabs.INSTR_Default("input", 300f, -100f, 150f, 50f);
-            dicoRender["NewGame"] = UIPrefabs.BTN_NewGame("NewGame", "New Game", 300f, 100f, 150f, 50f);
-            dicoRender["LoadGame"] = UIPrefabs.DDW_Default<LastSave>("LoadGame", "Load Game", 300f, 0f, 150f, 50f);
-
+            dicoRender["Quit"] = UIPrefabs.BTN_Quit("Quit", "Quit", 0f, -350f, 225f, 75f);
+            dicoRender["InputGameName"] = UIPrefabs.INSTR_Default("input", 0f, -150f, 225f, 75f);
+            dicoRender["NewGame"] = UIPrefabs.BTN_NewGame("NewGame", "New Game", 0f, -50f, 225f, 75f);
+            dicoRender["LoadGame"] = UIPrefabs.DDW_Default<LastSave>("LoadGame", "Load Game", 0f, -250f, 225f, 75f);
+            
             // Affichage des elements
             UIPrefabs.Render("UI_MenuCanvas", dicoRender);
         }
